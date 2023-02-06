899e242e340583e959e511451661b701868d51f9
Shivangi <shivangit1111@gmail.com>
2023-02-06 16:52:20 +0530

    desined bottom bar

--- app/src/main/java/com/example/pkaart/MainActivity.kt
+++ app/src/main/java/com/example/pkaart/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.pkaart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.PopupMenu
+import androidx.navigation.fragment.findNavController
+import com.example.pkaart.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer)
+        val navController = navHostFragment!!.findNavController()
+
+        val popupMenu = PopupMenu(this, null)
+        popupMenu.inflate(R.menu.bottom_nav)
+
+        binding.bottomBar.setupWithNavController(popupMenu.menu, navController)
+
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+
+
+            title = when (destination.id) {
+                R.id.cardFragment -> "MyCart"
+                R.id.moreFragment -> "MyDashBoard"
+                else -> " p-Kart"
+            }
+        }
     }
 }
