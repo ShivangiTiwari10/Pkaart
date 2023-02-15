3fa858dd68d9248c0ba057f6cbb8a19e125de0f5
Shivangi <shivangit1111@gmail.com>
2023-02-15 18:16:21 +0530

    totalCost shown by clicking on checkOut Button

--- app/src/main/java/com/example/pkaart/fragment/CardFragment.kt
+++ app/src/main/java/com/example/pkaart/fragment/CardFragment.kt
@@ -1,14 +1,18 @@
 package com.example.pkaart.fragment
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import com.example.pkaart.activity.AddressActivity
 import com.example.pkaart.adapter.cartAdapter
 import com.example.pkaart.databinding.FragmentCardBinding
 import com.example.pkaart.roomDb.AppDataBase
+import com.example.pkaart.roomDb.ProductModel
 
 
 class CardFragment : Fragment() {
@@ -33,9 +37,29 @@ class CardFragment : Fragment() {
 
         dao.getAllProducts().observe(requireActivity()) {
             binding.cartRecyclerView.adapter = cartAdapter(requireContext(), it)
+
+            totalCost(it)
         }
 
         return (binding.root)
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun totalCost(data: List<ProductModel>?) {
+        var total = 0
+        for (item in data!!) {
+            total += item.productSp!!.toInt()
+        }
+
+        binding.textTotal.text = "Total Item In Cart Is ${data.size}"
+        binding.textCost.text = "Total Cost:${total}"
+
+        binding.checkOut.setOnClickListener {
+            val intent = Intent(context, AddressActivity::class.java)
+            intent.putExtra("totalCost", total)
+            startActivity(intent)
+
+        }
     }
 
 
