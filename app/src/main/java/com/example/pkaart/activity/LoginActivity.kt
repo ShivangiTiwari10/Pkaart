13ec88a6d75044af7b7d3c6da7dfb6c0516d2755
Shivangi <shivangit1111@gmail.com>
2023-02-16 10:43:24 +0530

    Added login,register,otp activity

---
+++ app/src/main/java/com/example/pkaart/activity/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pkaart.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pkaart.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
