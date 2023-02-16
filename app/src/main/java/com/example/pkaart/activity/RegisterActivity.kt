7e52d5ca0b7308ba21d7499986cf28b6c912392d
Shivangi <shivangit1111@gmail.com>
2023-02-16 16:17:53 +0530

    validated user by name And Number & storedData,& Go login activity

--- app/src/main/java/com/example/pkaart/activity/RegisterActivity.kt
+++ app/src/main/java/com/example/pkaart/activity/RegisterActivity.kt
@@ -1,12 +1,72 @@
 package com.example.pkaart.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.pkaart.R
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.example.pkaart.databinding.ActivityRegisterBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.button2.setOnClickListener {
+            openLogin()
+        }
+
+        binding.button.setOnClickListener {
+            validateUser()
+        }
     }
+
+    private fun validateUser() {
+
+        if (binding.userName.text!!.isEmpty() || binding.userNumber.text!!.isEmpty())
+            Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show()
+        else storeData()
+    }
+
+    private fun storeData() {
+        val builder = AlertDialog.Builder(this)
+            .setTitle("Loading...")
+            .setMessage("Please Wait..")
+            .setCancelable(false)
+            .create()
+
+        builder.show()
+
+        val data = hashMapOf<String, Any>()
+
+        data["name"] = binding.userName.text.toString()
+        data["number"] = binding.userNumber.text.toString()
+
+        Firebase.firestore.collection("users").document(binding.userNumber.text.toString())
+            .set(data).addOnSuccessListener {
+                Toast.makeText(this, "User Register", Toast.LENGTH_SHORT).show()
+
+                openLogin()
+
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "Something went wrong", Toast.LENGTH_SHORT).show()
+            }
+
+    }
+
+    private fun openLogin() {
+
+        startActivity(Intent(this, LoginActivity::class.java))
+        finish()
+    }
+
+
 }
