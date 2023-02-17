2ff37a100eea040f40f20dcb23e5bf58bc18b90c
Shivangi <shivangit1111@gmail.com>
2023-02-17 15:18:52 +0530

    userModel added to get user name,Number,village,state,city

---
+++ app/src/main/java/com/example/pkaart/model/UserModel.kt
@@ -0,0 +1,11 @@
+package com.example.pkaart.model
+
+data class UserModel(
+
+    val userName: String? = "",
+    val userPhoneNumber: String? = "",
+    val village: String? = "",
+    val state: String? = "",
+    val city: String? = ""
+
+)
